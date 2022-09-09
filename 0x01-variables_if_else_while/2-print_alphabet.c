d558ff47ec2c82e5e915b9a620c6dad1c39ca099
Abdullahi Musa <musaabdullahi0398@gmail.com>
2022-09-09 14:17:55 -0700

    Print Alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,13 +5,15 @@
  * and uses the control loop for, and the function putchar
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	char ch;	
+	char ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
+	{
 		putchar(ch);
-
 		putchar('\n');
+	}
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Print Alphabet
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char a = 'a';
+	char b = 'b';
+
+	for (a = 'a'; a <= 'Z'; a++)
+	{
+		putchar(a);
+	}
+	for (b = 'A'; b <= 'Z'; b++)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+	return (0);
+}
