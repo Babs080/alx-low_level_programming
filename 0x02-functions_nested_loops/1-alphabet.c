e05fff048cc9ab5eba5b00d1f04d675fe8d505b9
Abdullahi Musa <musaabdullahi0398@gmail.com>
2022-09-13 04:24:12 -0700

    Lowercase Alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main -  Print alphabet in lowercase using only `_putchar`
+ * followed by a new line.
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
